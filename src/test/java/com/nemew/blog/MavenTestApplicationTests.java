415b55119d432c7c739fa4e6c710c9468df69450
송현주 <vjzm44@naver.com>
2020-01-08 22:30:35 +0900

    현주개인pc연동
    연동완료...인터넷망pc에도 돌아가야할텐데

---
+++ src/test/java/com/nemew/blog/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemew.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
